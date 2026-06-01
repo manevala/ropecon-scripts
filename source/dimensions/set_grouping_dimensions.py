a9fbdee796a5ddd87a59529d6316d76661e9d883
Maija Nevala <>
2026-06-01 21:55:18 +0300

    Add limit parameter

--- source/dimensions/set_grouping_dimensions.py
+++ source/dimensions/set_grouping_dimensions.py
@@ -1,13 +1,23 @@
+import argparse
 import os
 
 from dotenv import load_dotenv
 
-from dimensions import program_client
 from dimensions.program_client import ProgramClient
 from graphql_client.graphql_client import Auth
 
 
 def set_grouping_dimensions():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "-l",
+        "--limit",
+        help="set dimensions only for the first LIMIT programs found. Use to test that all works as expected",
+        type=int,
+    )
+    args = arg_parser.parse_args()
+    limit = args.limit
+
     load_dotenv()
     client = ProgramClient(
         os.environ["KOMPASSI_GRAPHQUL_URL"],
@@ -18,8 +28,13 @@ def set_grouping_dimensions():
 
     print(f"Found {len(programs_to_edit)} programs to edit.")
 
+    if limit is not None:
+        programs_to_edit = programs_to_edit[:limit]
+        print(f"NOTE: editing only {limit} programs")
+
     print("Starting set_grouping_dimensions...")
     for program in programs_to_edit:
+        print(f"Setting dimensions for program {program.program_id}")
         client.set_correct_dimension_value(program)
     print("Done!")
 
